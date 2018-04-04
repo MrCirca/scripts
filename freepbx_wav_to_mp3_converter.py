fe49ddd6641e3a64136a10eb31e2fd2f8e78ca14
MrCirca <geortsir3@gmail.com>
2018-04-04 21:12:31 +0300

    Add script that converts wav files to mp3 and change the extension in database too

---
+++ freepbx_wav_to_mp3_converter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import subprocess
+import os.path
+
+record_files =  subprocess.check_output(['find', '/var/spool/asterisk/monitor/', '-type', 'f', '-name', '*.wav']).decode()
+
+for wav_file in record_files.splitlines():
+	name, ext = os.path.splitext(wav_file)
+	mp3_file = "{}.mp3".format(name)
+	subprocess.check_output(['ffmpeg', '-i', wav_file, mp3_file, '-y'])
+	os.remove(wav_file)
+
+try:
+    conn = MySQLdb.connect(host="localhost", db="asteriskcdrdb")
+    cursor = conn.cursor()
+
+except Exception as e:
+    error = True
+
+cursor.execute("SELECT uniqueid,recordingfile FROM cdr")
+result = cursor.fetchall()
+for unique_id, record_file in result:
+    name, ext = os.path.splitext(record_file)
+    if ext == ".wav":
+        print(ext)
+        cursor.execute("UPDATE cdr SET recordingfile='{}.mp3'".format(name)+" WHERE uniqueid='{}'".format(unique_id))
+        conn.commit()
+
