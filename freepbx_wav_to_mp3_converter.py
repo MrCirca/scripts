de55d93eacec2ff7e7e7b4d21a7d8d1ef39c910c
MrCirca <geortsir3@gmail.com>
2018-04-11 17:59:46 +0300

    Add argument, converting format of your choice

--- freepbx_wav_to_mp3_converter.py
+++ freepbx_wav_to_mp3_converter.py
@@ -3,13 +3,15 @@
 import MySQLdb
 import subprocess
 import os.path
+import sys
 
+file_format = sys.argv[1]
 record_files =  subprocess.check_output(['find', '/var/spool/asterisk/monitor/', '-type', 'f', '-name', '*.wav']).decode()
 
 for wav_file in record_files.splitlines():
 	name, ext = os.path.splitext(wav_file)
-	mp3_file = "{}.mp3".format(name)
-	subprocess.check_output(['ffmpeg', '-i', wav_file, mp3_file, '-y'])
+	prefer_format_file = "{}.".format(name)+file_format
+	subprocess.check_output(['ffmpeg', '-i', wav_file, prefer_format_file, '-y'])
 	os.remove(wav_file)
 
 try:
@@ -25,6 +27,6 @@ for unique_id, record_file in result:
     name, ext = os.path.splitext(record_file)
     if ext == ".wav":
         print(ext)
-        cursor.execute("UPDATE cdr SET recordingfile='{}.mp3'".format(name)+" WHERE uniqueid='{}'".format(unique_id))
+        cursor.execute("UPDATE cdr SET recordingfile='{}.".format(name) + file_format + "'" + " WHERE uniqueid='{}'".format(unique_id))
         conn.commit()
 
