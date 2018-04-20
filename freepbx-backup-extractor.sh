c56c3d26302d64808c280f234bccd706e1cbf0a4
MrCirca <geortsir3@gmail.com>
2018-04-20 22:09:51 +0300

    Remove variables

--- freepbx-backup-extractor.sh
+++ freepbx-backup-extractor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-LATEST_BACKUP_FILE="$(ls -tr /var/spool/asterisk/backup/Full_Backup/*.tgz | tail -1)"
+LATEST_BACKUP_FILE="$(ls -tr /var/spool/asterisk/backup/test/*.tgz | tail -1)"
 EXTRACTED_DIRECTORY="/var/spool/asterisk/backup/Full_Backup/last_backup_extracted"
 
 if [ ! -d "$EXTRACTED_DIRECTORY" ]; then
-	CREATE_EXTRACTED_DIRECTORY=$(mkdir "$EXTRACTED_DIRECTORY")
+	mkdir "$EXTRACTED_DIRECTORY"
 else
-	echo "File Exist"
+	echo "Directory exists"
 fi
-DECOMPRESS_BACKUP_FILE=$(tar xzf "$LATEST_BACKUP_FILE" -C "$EXTRACTED_DIRECTORY")
-EXTRACT_DATABASES="$(gunzip "$EXTRACTED_DIRECTORY/mysql-2.sql.gz" && gunzip "$EXTRACTED_DIRECTORY/mysql-3.sql.gz")"
+tar xzf "$LATEST_BACKUP_FILE" -C "$EXTRACTED_DIRECTORY"
+gunzip $EXTRACTED_DIRECTORY/mysql-*.sql.gz
--- freepbx-backup-unextractor.sh
+++ freepbx-backup-unextractor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 PATH_GIVEN=$1
-COMPRESS_DATABASES="$(gzip "$PATH_GIVEN/mysql-2.sql" && gzip "$PATH_GIVEN/mysql-3.sql")"
-COMPRESS_BACKUP_DIRECTORY="$(tar -Pzcf "$PATH_GIVEN.tgz" "$PATH_GIVEN")"
+gzip $PATH_GIVEN/mysql-*.sql
+tar -Pczf $PATH_GIVEN.tgz -C $PATH_GIVEN .
