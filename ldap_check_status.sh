e0c3ec415039b3e4ff9fbb1d7ad14e1bd861c882
MrCirca <geortsir3@gmail.com>
2018-07-19 15:46:10 +0300

    Add base_dn searching database and redirect stderr to /dev/null fs

--- ldap_check_status.sh
+++ ldap_check_status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-BASE_DN=$1
-PROVIDER_URI=$2
-LDAP_CSN_CONSUMER_COMMAND=$(ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -s base -b "$BASE_DN" contextCSN 2>&1)
+BASE_DN=$(ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -b cn=config 2> /dev/null | grep "olcSuffix:" | cut -d " " -f 2)
+PROVIDER_URI=$1
+LDAP_CSN_CONSUMER_COMMAND=$(ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -s base -b "$BASE_DN" contextCSN 2> /dev/null)
 LDAP_CSN_CONSUMER_RC=$?
-LDAP_CSN_PROVIDER_COMMAND=$(ldapsearch -LLL -x -H ldaps://"$PROVIDER_URI" -s base -b "$BASE_DN" contextCSN 2>&1)
+LDAP_CSN_PROVIDER_COMMAND=$(ldapsearch -LLL -x -H ldaps://"$PROVIDER_URI" -s base -b "$BASE_DN" contextCSN 2> /dev/null)
 LDAP_CSN_PROVIDER_RC=$?
 PROVIDER_CSN=$(echo -e $LDAP_CSN_PROVIDER_COMMAND | grep contextCSN | cut -d " " -f 2)
 CONSUMER_CSN=$(echo -e $LDAP_CSN_CONSUMER_COMMAND | grep contextCSN | cut -d " " -f 2)
