ad5de1948a45d45d4a9d256d55c65a08592b9c1e
Giorgos Tsirkas <gtsirkas@modulus.gr>
2018-11-26 20:02:02 +0200

    Fix typo

--- supervisor_services_monitor.py
+++ supervisor_services_monitor.py
@@ -8,15 +8,15 @@ nargs = len(sys.argv)
 
 if nargs == 1:
     services = []
-    supervisor_services = sp.check_output(["supervisorctl", "status"]).decode()
+    supervisor_services = sp.check_output(["sudo", "supervisorctl", "status"]).decode()
     for line in supervisor_services.splitlines():
         service_name = line.split()[0]
-        services.append({"#SUPERVISOR_SERVICE": service_name })
+        services.append({"{#SUPERVISOR_SERVICE}": service_name })
     print(json.dumps({'data' : services }))
 
 elif nargs == 3:
     supervisor_service = sys.argv[1]
-    service_status = sp.check_output(["supervisorctl", "status", supervisor_service]).decode()
+    service_status = sp.check_output(["sudo", "supervisorctl", "status", supervisor_service]).decode()
     if "(no such process)" in service_status:
         print("Process does not exist")
     else:
@@ -25,10 +25,10 @@ elif nargs == 3:
             print(service_uptime)
         elif sys.argv[2] == "state":
             service_state = service_status.split()[1]
-	    if service_state == "RUNNING":
-		print("1")
-	    elif service_state == "STOPPED":
-		print("0")
+            if service_state == "RUNNING":
+                print("1")
+            elif service_state == "STOPPED":
+                print("0")
             else:
                 print("10")
         else:
