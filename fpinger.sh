c7ff2fd28cdefa22fa9d12f5ab69e3acca7f2bc3
Giorgos Tsirkas <gtsirkas@modulus.gr>
2019-05-28 21:29:57 +0300

    Fix structure of fpinger.sh

--- fpinger.sh
+++ fpinger.sh
@@ -1,23 +1,33 @@
-#/bin/bash
+#/bin/sh
 
 FPING_TARGET=$2
 FPING_COUNT=$3
 FPING_PERIOD=$4
 FPING_BYTES_SIZE=$5
 FPING_TIMEOUT=$6
-FPING_SRC=$7
-USER_PARAM=$8
+FPING_SRC_IP=$7
+FPING_METRIC=$8
+FPING_LATENCY_FUNC=$9
 
-ZABBIX_SENDER_COMMAND="zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i - >/dev/null | at now &>/dev/null"
 
-FPING_COMMAND="fping -q -c $FPING_COUNT -p $FPING_PERIOD -b $FPING_BYTES_SIZE -t $FPING_TIMEOUT -S $FPING_SRC $FPING_TARGET"
+FPING_COMMAND="fping -q -c $FPING_COUNT -p $FPING_PERIOD -b $FPING_BYTES_SIZE -t $FPING_TIMEOUT -S $FPING_SRC_IP $FPING_TARGET"
 
-if [ $8 == "agent_ping" ]; then
-    echo $($FPING_COMMAND 2> /dev/null && echo 1 || echo 0) | sed -r "s/.*/- agentping_trap[$1] \0/g"
+ZABBIX_SENDER_COMMAND="zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i -"
+
+if [ $8 == "availability" ]; then
+    COMMAND="($FPING_COMMAND 2> /dev/null && echo 1 || echo 0)"
+    ZABBIX_TRAP_ITEM_KEY="agentping_availability_trap"
 
 elif [ $8 == "packet_loss" ]; then
-    echo $($FPING_COMMAND 2>&1 | egrep -o "([0-9]+\.)?[0-9]+%" | tr -d "%" | sed -r "s/.*/- agentpingloss_trap[$1] \0/g")
+    COMMAND="$FPING_COMMAND 2>&1 | egrep -o \"([0-9]+\.)?[0-9]+%\" | tr -d "%""
+    ZABBIX_TRAP_ITEM_KEY="agentping_packet_loss_trap"
 
-elif [ $8 == "sec" ]; then
-    echo $($FPING_COMMAND 2>&1 | (egrep -o "(([0-9]+\.)?[0-9]+/?)+$" || echo "0") | ( [[ "$9" == "min" ]] && cut -d "/" -f 1 || [[ "$9" == "avg" ]] && cut -d "/" -f 2 || [[ "$9" == "max" ]] && cut -d "/" -f 3) | sed -r "s/.*/- agentpingsec_trap[min] \0/g")
+elif [ $8 == "latency" ]; then
+    ZABBIX_TRAP_ITEM_KEY="agentping_latency_trap"
+    COMMAND="$FPING_COMMAND 2>&1 | (egrep -o \"(([0-9]+\.)?[0-9]+/?)+$\" || echo "0") | ( [[ "$FPING_LATENCY_FUNC" == "min" ]] && cut -d "/" -f 1 || [[ "$FPING_LATENCY_FUNC" == "avg" ]] && cut -d "/" -f 2 || [[ "$FPING_LATENCY_FUNC" == "max" ]] && cut -d "/" -f 3)"
 fi
+
+COMMAND="$COMMAND | sed -r 's/.*/- $ZABBIX_TRAP_ITEM_KEY[$1] \0/g'"
+#COMMAND="$COMMAND | $ZABBIX_SENDER_COMMAND"
+
+echo $COMMAND
